120e23fbda7f3db70859119d478db3bbca0970c7
Jack <superjackpower85@gmail.com>
2019-07-22 16:34:28 +0800

    initial commit

--- D3_exercises/01-Click-me/lib/index.js
+++ D3_exercises/01-Click-me/lib/index.js
@@ -1,8 +1,12 @@
 const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
-
+  var x = document.getElementsByClassName("btn");
   // TODO: Bind the `click` event to the button
   // TODO: On click, display `Thank you!` in a JavaScript alert!
+  
+  x.addEventListener("click", displayAlertOnButtonClick);
+  alert("Thank you");
+
 };
 
 displayAlertOnButtonClick(); // Do not remove!
