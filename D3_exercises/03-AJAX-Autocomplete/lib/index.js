6b6765cb6baf8918f3f2f5285e3c8c8268776f5d
Jack <superjackpower85@gmail.com>
2019-07-23 08:13:53 +0800

    autocomplete box

--- D3_exercises/03-AJAX-Autocomplete/lib/index.js
+++ D3_exercises/03-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,30 @@
 // TODO: Autocomplete the input with AJAX calls.
+// function clear() {
+//   document.getElementsByTagName("li").innerHTML = "";
+// }
+
+// document.getElementById("results").onclick = function() { 
+//   clear(); 
+// } 
+// var ourRequest = new XMLHttpRequest();
+// ourRequest.open("GET", "https://recode-dictionary.herokuapp.com/autocomplete/:stem");
+// ourRequest.onload = function(){
+//   var ourData = JSON.parse(ourRequest.responseText);
+//   renderHTML(ourData);
+// };
+// ourRequest.send();
+
+// function renderHTML(data){
+
+// }
+var ourRequest = new XMLHttpRequest();
+var ids = document.getElementById("results");
+
+xhr.onload = function (){
+  if (this.status === 200){
+    ids = ids.responseText;
+  }
+}
+
+xhr.open("GET", "https://recode-dictionary.herokuapp.com/autocomplete/", true);
+xhr.send();
